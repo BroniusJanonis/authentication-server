411cecc632ad7ff1a8aa85aabd592a38ce519718
Bronius <Bronius.Janonis@inntec.lt>
2018-10-09 09:56:03 +0300

    1. Created authentication-server 2. Uses facebook and github to authenticate 3. Authentications so far based on another person's authentication from facebook and githun 4. Source tutorial:  https://spring.io/guides/tutorials/spring-boot-oauth2/#_social_login_authserver

---
+++ src/main/java/authentication/server/api/AuthenticationController.java
@@ -0,0 +1,20 @@
+package authentication.server.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+public class AuthenticationController{
+
+    @RequestMapping({ "/user", "/me" })
+    public Map<String, String> user(Principal principal) {
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("name", principal.getName());
+        return map;
+    }
+
+}
